a3adfe59f64cc8432e5c9010147d4bb081f2c490
Maxim Litvinov <malitvin@ncsu.edu>
2018-12-19 15:08:19 -0500

    Grenade Launcher

---
+++ Source/CoopGame/Private/SGrenadeLauncher.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SGrenadeLauncher.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
+
+void ASGrenadeLauncher::Fire()
+{
+	AActor* MyOwner = GetOwner();
+	if (MyOwner && ProjectileClass)
+	{
+		FVector EyeLocation;
+		FRotator EyeRotation;
+
+		MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
+		FVector MuzzleLocation = MeshComp->GetSocketLocation(MuzzleSocketName);
+		//FVector MuzzleRotation = MeshComp->GetSocketRotation(MuzzleSocketName);
+
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		GetWorld()->SpawnActor<AActor>(ProjectileClass,MuzzleLocation,EyeRotation,SpawnParams);
+	}
+}
